c09ce852f58060ccb65ee3244f088f3e2651c6ba
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-06-28 17:05:12 -0300

    registrado router de cursos

--- course_service/app/main.py
+++ course_service/app/main.py
@@ -1,50 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
+from fastapi import FastAPI
+
+from api.courses import courses
 
 app = FastAPI()
 
-fake_course_db = [
-    {
-        'name': 'Python',
-        'description': 'Introduction to Programming',
-        'students': ['Marcelo Ceolin', 'Jean C']
-    }
-]
-
-
-class Course(BaseModel):
-    name: str
-    description: str
-    students: List[str]
-
-
-@app.get('/', response_model=List[Course])
-async def index():
-    return fake_course_db
-
-
-@app.post('/', status_code=201)
-async def add_course(payload: Course):
-    course = payload.dict()
-    fake_course_db.append(course)
-    return {'id': len(fake_course_db) - 1}
-
-
-@app.put('/{id')
-async def update_course(id: int, payload: Course):
-    course = payload.dict()
-    courses_length = len(fake_course_db)
-    if 0 <= id <= courses_length:
-        fake_course_db[id] = course
-        return None
-    raise HTTPException(status_code=404, detail='Course with given id is not found')
-
-
-@app.delete('/{id}')
-async def delete_course(id: int):
-    courses_length = len(fake_course_db)
-    if 0 <= id <= courses_length:
-        del fake_course_db[id]
-        return None
-    raise HTTPException(status_code=404, detail='Course with given id is not found')
+app.include_router(courses)
