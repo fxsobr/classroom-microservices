2b3c8a03662ba744cc300a80797f045d99125703
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-06-28 18:12:42 -0300

    criado funcões que verificam o banco em startup e shutdown

--- course_service/app/main.py
+++ course_service/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
 from course_service.app.api.courses import courses
+from course_service.app.api.db import metadata, engine, database
+
+metadata.create_all(engine)
 
 app = FastAPI()
+
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
+
 
 app.include_router(courses)
